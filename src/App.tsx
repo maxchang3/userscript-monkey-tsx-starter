dea19638218b593731b0e694bf6997ed090c9ff9
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2025-05-06 23:33:05 +0800

    feat: add Counter component and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import styles from './App.module.css'
 
+const Counter = () => {
+    const count = React.createRef()
+    const onClick = () => count.current.innerText++
+
+    return (
+        <div>
+            <h1>Counter</h1>
+            <p ref={count}>0</p>
+            <button onClick={onClick} type="button" class={styles.button}>
+                Increment
+            </button>
+        </div>
+    )
+}
+
 const App = () => {
     return (
         <div class={styles.App}>
             <header class={styles.header}>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
+                <Counter />
             </header>
         </div>
     )
