689c40bfb8bff47b050df699bb4fb327b6f7252d
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2025-05-06 23:52:27 +0800

    chore: update userscript match pattern to allow subpaths

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
             userscript: {
                 icon: 'https://vitejs.dev/logo.svg',
                 namespace: 'npm/vite-plugin-monkey',
-                match: ['https://www.google.com/'],
+                match: ['https://www.google.com/*'],
             },
             build: {
                 externalGlobals: {
